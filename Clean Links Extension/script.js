dcad4a50cb89d4645999a705d700397b2ad48334
Radoslav Vitanov <radoslav.vitanov@icloud.com>
2020-03-13 13:55:05 +0200

    Initial commit

--- Clean Links Extension/script.js
+++ Clean Links Extension/script.js
@@ -1,3 +1,49 @@
+var queryParamsToRemove = [
+    "fbclid",
+    "utm_source",
+    "utm_medium",
+    "utm_campaign",
+    "utm_term",
+    "utm_content",
+    "gclid",
+    "gclsrc",
+    "dclid",
+    "zanpid"
+];
+
+function catchLinks (root, cb) {
+    root.addEventListener('click', function (ev) {
+        var anchor = null;
+        for (var n = ev.target; n.parentNode; n = n.parentNode) {
+            if (n.nodeName === 'A') {
+                anchor = n;
+                break;
+            }
+        }
+        if (!anchor) return true;
+        
+        ev.preventDefault();
+
+        var clean = cleanUrl(anchor.href);
+        cb(clean, (ev.metaKey || ev.ctrlKey ));
+        return false;
+    });
+};
+
+function cleanUrl(url) {
+    var parsedUrl = new URL(url);
+    
+    for(var param of queryParamsToRemove) {
+        if (parsedUrl.searchParams.has(param)) {
+            parsedUrl.searchParams.delete(param);
+        }
+    }
+
+    return parsedUrl.href;
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+    catchLinks(window, function (href, external) {
+        window.open(href, external ? '_blank' : '_self','noopener,noreferrer');
+    });
 });
